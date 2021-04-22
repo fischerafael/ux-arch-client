4be0a66f2befa190a692a02879ce9253b4b9f622
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-04-22 16:43:10 -0300

    change api url to production

--- src/services/url/url.ts
+++ src/services/url/url.ts
@@ -1,5 +1,6 @@
 const BASE_URL = {
-    dev: 'http://localhost:1337'
+    dev: 'http://localhost:1337',
+    production: 'https://ux-arch-strapi.herokuapp.com'
 }
 
-export const API_URL = BASE_URL.dev
+export const API_URL = BASE_URL.production
